cbe00fb5c6e8a5a631710c4f42b6e9cc0327157d
n58past <njpaster@iu.edu>
2023-08-28 19:42:20 -0400

    Finish assignments

---
+++ one_two.java
@@ -0,0 +1,9 @@
+public class one_two {
+    public static void main(String[] args){
+            System.out.println("Welcome to Java");
+            System.out.println("Welcome to Java");
+            System.out.println("Welcome to Java");
+            System.out.println("Welcome to Java");
+            System.out.println("Welcome to Java");
+    }
+}
