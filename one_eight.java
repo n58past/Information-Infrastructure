4234b7517087d29854029893ac6c58bdca227409
n58past <njpaster@iu.edu>
2023-09-05 20:02:58 -0400

    HW 2

--- one_eight.java
+++ one_eight.java
@@ -1,8 +1,8 @@
 public class one_eight{
     public static void main(String[] args){
-        System.out.print("The perimiter is ");
+        System.out.print("The perimiter of a circle with radius of 5.5 is ");
         System.out.println(2 * 5.5 * Math.PI);
-        System.out.print("The area is ");
+        System.out.print("The area of a circle with radius of 5.5 is ");
         System.out.println(5.5 * 5.5 * Math.PI);
     }
 }
--- one_six.java
+++ one_six.java
@@ -1,5 +1,6 @@
 public class one_six {
     public static void main(String[] args){
-        System.out.println(1+2+3+4+5+6+7+8+9);
+        System.out.print("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = ");
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
     }
 }
